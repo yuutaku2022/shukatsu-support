064717c736dbb14a45e6a6ed128343301f092a55
Yuuta <yuutaman0424ku@gmail.com>
2025-06-29 15:11:53 +0900

    commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dashBord import show_dashboard
 from organization_create import show_org_create
 from organization_detail import show_org_detail
 from event_make import show_event_create
+from organization_edit import show_org_edit
 from login import login
 
 #初期ページ設定
@@ -21,5 +22,7 @@ elif st.session_state.page == "event_make":
 	show_event_create()
 elif st.session_state.page == "dashboard":
 	show_dashboard()
+elif st.session_state.page == "org_edit":
+	show_org_edit()
 else:
 	login()
