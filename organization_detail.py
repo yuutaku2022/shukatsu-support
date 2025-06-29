e9b780a1fe3db2ef3fb0bb07aa3bdba524478326
Yuuta <yuutaman0424ku@gmail.com>
2025-06-29 11:43:48 +0900

    commit

---
+++ organization_detail.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import os
+def show_org_detail():
+	#csv読み込み
+	DATA_DIR = "data"
+
+
+	#編集画面を表示
+	if st.button('企業情報の編集'):
+		st.session_state.page = "edit"
+	
+	company = st.session_state.get("selected_company")
+	if not company:
+		st.warning("企業データが見つかりません")
+		if st.button('企業一覧に戻る'):
+			st.session_state.page = "dashboard"
+	
+	st.title(f"{company['企業名']}")
+	st.markdown(f'<a href="{company["マイページURL"]}" target="_blank">マイページへ</a>', unsafe_allow_html=True)
+	st.write(f"ID：{company['ID']}")
+	st.write(f"パスワード：{company['パスワード']}")
+
+	st.subheader('予定されたイベント')
+	if st.button('イベントを追加'):
+		st.session_state.page = "event_make"
+
+	
+
+
+
+	if st.button('戻る'):
+		st.session_state.page = "dashboard"
+
+
+
+	
